03c68510af4ed04add8b02da89eb834136cba36b
Nerrow <memeboys1337@gmail.com>
2021-02-27 21:51:31 +0700

    Init commit

---
+++ src/main/java/ru/shift/certbotcft/CertbotcftApplication.java
@@ -0,0 +1,13 @@
+package ru.shift.certbotcft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertbotcftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertbotcftApplication.class, args);
+    }
+
+}
